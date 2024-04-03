dcab1fe94115122a3652a9e9e98af56336b0fdd9
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-04-02 20:51:40 -0400

    relative line numbers

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -574,6 +574,7 @@ require('lazy').setup({
         prettier = {},
         pyright = {},
         rust_analyzer = {},
+        omnisharp_mono = {},
       }
 
       -- Ensure the servers and tools above are installed
