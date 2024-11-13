a54d69c808e7dbf1893da9da655751419e7a37a0
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-11-12 23:26:05 -0500

    update lsps and which-key

--- init.lua
+++ init.lua
@@ -299,17 +299,25 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require 'which-key'
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]Harpoon', _ = 'which_key_ignore' },
-        ['<leader>F'] = { name = '[F]Flutter', _ = 'which_key_ignore' },
+      wk.add {
+        { '<leader>F', group = '[F]Flutter' },
+        { '<leader>F_', hidden = true },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>h', group = '[H]Harpoon' },
+        { '<leader>h_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
       }
     end,
   },
@@ -595,7 +603,7 @@ require('lazy').setup({
         deno = {},
         grammarly = {},
         prettier = {},
-        ruff_lsp = { capabilities = { hoverProvider = false } },
+        ruff = {},
         pyright = {},
         rust_analyzer = {},
         jdtls = {},
